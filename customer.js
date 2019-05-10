5a08ebde18200c4e3ac6e8583253f6683d0c9ba4
kbub27 <kalebmerriman27@gmail.com>
2019-05-09 20:29:06 -0700

    query for product stock to show the customer

--- customer.js
+++ customer.js
@@ -2,15 +2,34 @@ var sql = require('mysql');
 var inq = require('inquirer');
 var sqlPass = require('./pass.js');
 
-sql.createConnection({
-    host: 'localhost',
+// CREATE FUNCTION TO DISPLAY THE DATA FROM THE DB THEN PROMPT QUESTIONS
+var customer = () => {
+    var con = sql.createConnection({
+        host: 'localhost',
 
-    port: '3306',
+        port: '3306',
 
-    user: 'root',
+        user: 'root',
 
-    password: sqlPass,
+        password: sqlPass,
 
-    database: 'bamazonDB'
-});
+        database: 'bamazonDB'
+    });
+    //SHOW ITEMS IN STOCK TO THE CUSTOMER
+    con.query('select * from products', function (err, res) {
+        if (err) throw err;
+        console.log('\n-----------PRODUCTS IN STOCK------------\n');
+        for (let i = 0; i < res.length; i++) {
+            console.log(
+                'ID: ' + res[i].item_id,
+                'Product: ' + res[i].product_name,
+                'Price: ' + res[i].price,
+                'In-Stock: ' + res[i].stock_quantity
+                );
+        }
+    });
+    // PROMPT THE CUSTOMER WITH QUESTIONS AND RETURN THE RESULTS THEY WANT
+    
+}
 
+customer();
