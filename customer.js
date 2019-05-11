4b00ec7ec6b2258de625588e741e68be310ca4f0
kbub27 <kalebmerriman27@gmail.com>
2019-05-10 21:36:28 -0700

    fixed scope issue in customer function and added buyAgain function to prompt and allow the user to buy again if needed

--- customer.js
+++ customer.js
@@ -15,25 +15,25 @@ var con = sql.createConnection({
 });
 
 //CREATE A FUNCTION FOR CUSTOMER PROMPT
-var cusPrompt = () => {
+var cusPrompt = (res) => {
     var questions = [
         {
             name: 'id',
             type: 'input',
             message: 'What is the id of the item you would like to buy?',
             validate: function (value) {
-                if(!isNaN(value)) {
+                if (!isNaN(value)) {
                     return true;
                 } else {
                     return false
-                }
+                };
             },
         },
         {
             name: 'units',
             type: 'input',
             validate: function (value) {
-                if(!isNaN(value)) {
+                if (!isNaN(value)) {
                     return true;
                 } else {
                     return false
@@ -46,29 +46,80 @@ var cusPrompt = () => {
     inq.prompt(questions).then(answers => {
         var Id = answers.id;
         var Units = answers.units;
+        var remainingStock = Math.floor(res[Math.floor(Id - 1)].stock_quantity - Units);
         //QUERY FOR SELECTED ID 
         con.query('select item_id, product_name, price, stock_quantity from products where item_id = ?', Id, function (err, response) {
             if (err) throw err;
-            console.log('\n====================Purchased Item===========================\n');
+
+            console.log('\n===========================Ordered Item=============================\n');
             console.table(response);
             console.log('================================================================\n');
-        })
+            if (Units > response[0].stock_quantity) {
+                console.log('We do not have that many units in stock, sorry');
+                customer();
+            } else {
+                console.log(
+                    'You have Ordered ' + Units + ' ' +
+                    response[0].product_name + '\n' +
+                    'Your Total is : $' + response[0].price * Units +
+                    '\nItems will be shipped as soon as payment is processed!\n'
+                );
+                // UPDATE THE DATABSE HERE
+                con.query(
+                    'update products set ? where ?',
+                    [
+                        {
+                            stock_quantity: remainingStock
+                        },
+                        {
+                            item_id: Id
+                        }
+                    ],
+                    function (err) {
+                        if (err) throw err
+                    });
+
+                con.query('select * from products', function (err, res) {
+                    if (err) throw err;
+
+                    console.log('====================Products in stock===========================\n');
+                    console.table(res);
+                    console.log('================================================================\n');
+                    buyAgain();
+                });
+            }
+        });
     })
 }
 
 // CREATE FUNCTION TO DISPLAY THE DATA FROM THE DB THEN PROMPT QUESTIONS
 var customer = () => {
-    
+
     //SHOW ITEMS IN STOCK TO THE CUSTOMER
     con.query('select * from products', function (err, res) {
         if (err) throw err;
-        
+
         console.log('====================Products in stock===========================\n');
         console.table(res);
         console.log('================================================================\n');
-        cusPrompt();
+        cusPrompt(res);
     });
-    // PROMPT THE CUSTOMER WITH QUESTIONS AND RETURN THE RESULTS THEY WANT
+};
+
+var buyAgain = () => {
+    inq.prompt([
+        {
+            type: 'confirm',
+            name: 'again',
+            message: 'Would you like to purchase another item?'
+        }
+    ]).then(answer => {
+        if (answer.again) {
+            customer();
+        } else {
+            con.end();
+        }
+    })
 }
 
 customer();
