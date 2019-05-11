94620c7ec3fc5467b2eaa71d7d5d96baee555fa6
kbub27 <kalebmerriman27@gmail.com>
2019-05-10 19:28:57 -0700

    added questions for prompt and added a console.table npm package

--- customer.js
+++ customer.js
@@ -1,6 +1,33 @@
 var sql = require('mysql');
 var inq = require('inquirer');
 var sqlPass = require('./pass.js');
+var table = require('console.table');
+
+//CREATE A FUNCTION FOR CUSTOMER PROMPT
+var cusPrompt = (queryResults) => {
+    var questions = [
+        {
+            name: 'id',
+            type: 'list',
+            choices: results,
+            message: 'What is the id of the item you would like to buy?'
+        },
+        {
+            name: 'units',
+            type: 'input',
+            validate: function (value) {
+                if(!isNaN(value)) {
+                    return true;
+                } else {
+                    return false
+                }
+            },
+            message: 'How many units would you like to buy?'
+        }
+    ];
+
+
+}
 
 // CREATE FUNCTION TO DISPLAY THE DATA FROM THE DB THEN PROMPT QUESTIONS
 var customer = () => {
@@ -18,19 +45,16 @@ var customer = () => {
     //SHOW ITEMS IN STOCK TO THE CUSTOMER
     con.query('select * from products', function (err, res) {
         if (err) throw err;
-        console.log('\n-----------PRODUCTS IN STOCK------------\n');
+        var results = [];
         for (let i = 0; i < res.length; i++) {
-            console.log(
-                'ID: ' + res[i].item_id,
-                'Product: ' + res[i].product_name,
-                'Price: ' + res[i].price,
-                'In-Stock: ' + res[i].stock_quantity
-                );
+            results.push(res[i].item_id)
         };
-        console.log('==============================================')
+        console.log('====================Products in stock===========================\n');
+        console.table(res);
+        console.log('================================================================\n');
+        cusPrompt(results);
     });
     // PROMPT THE CUSTOMER WITH QUESTIONS AND RETURN THE RESULTS THEY WANT
-
 }
 
 customer();
