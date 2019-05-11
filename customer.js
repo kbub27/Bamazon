9cb43513a755dd580b94d81fcd5ac2c38ceb4278
kbub27 <kalebmerriman27@gmail.com>
2019-05-10 19:59:05 -0700

    added the log and query of selected id

--- customer.js
+++ customer.js
@@ -2,15 +2,32 @@ var sql = require('mysql');
 var inq = require('inquirer');
 var sqlPass = require('./pass.js');
 var table = require('console.table');
+var con = sql.createConnection({
+    host: 'localhost',
+
+    port: '3306',
+
+    user: 'root',
+
+    password: sqlPass,
+
+    database: 'bamazonDB'
+});
 
 //CREATE A FUNCTION FOR CUSTOMER PROMPT
-var cusPrompt = (queryResults) => {
+var cusPrompt = () => {
     var questions = [
         {
             name: 'id',
-            type: 'list',
-            choices: results,
-            message: 'What is the id of the item you would like to buy?'
+            type: 'input',
+            message: 'What is the id of the item you would like to buy?',
+            validate: function (value) {
+                if(!isNaN(value)) {
+                    return true;
+                } else {
+                    return false
+                }
+            },
         },
         {
             name: 'units',
@@ -26,33 +43,30 @@ var cusPrompt = (queryResults) => {
         }
     ];
 
-
+    inq.prompt(questions).then(answers => {
+        var Id = answers.id;
+        var Units = answers.units;
+        //QUERY FOR SELECTED ID 
+        con.query('select item_id, product_name, price, stock_quantity from products where item_id = ?', Id, function (err, response) {
+            if (err) throw err;
+            console.log('\n====================Purchased Item===========================\n');
+            console.table(response);
+            console.log('================================================================\n');
+        })
+    })
 }
 
 // CREATE FUNCTION TO DISPLAY THE DATA FROM THE DB THEN PROMPT QUESTIONS
 var customer = () => {
-    var con = sql.createConnection({
-        host: 'localhost',
-
-        port: '3306',
-
-        user: 'root',
-
-        password: sqlPass,
-
-        database: 'bamazonDB'
-    });
+    
     //SHOW ITEMS IN STOCK TO THE CUSTOMER
     con.query('select * from products', function (err, res) {
         if (err) throw err;
-        var results = [];
-        for (let i = 0; i < res.length; i++) {
-            results.push(res[i].item_id)
-        };
+        
         console.log('====================Products in stock===========================\n');
         console.table(res);
         console.log('================================================================\n');
-        cusPrompt(results);
+        cusPrompt();
     });
     // PROMPT THE CUSTOMER WITH QUESTIONS AND RETURN THE RESULTS THEY WANT
 }
